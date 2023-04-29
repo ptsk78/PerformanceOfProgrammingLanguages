455b434e77116d032d480c89c4ff8fa58c866c03
Peter Taraba <115673006+ptsk78@users.noreply.github.com>
2023-04-29 14:54:11 +0200

    Add files via upload

---
+++ compareos.py
@@ -0,0 +1,35 @@
+from os import listdir
+from os.path import isfile, join
+
+osfiles = [f for f in listdir('./results_os/') if isfile(join('./results_os/', f)) and f.endswith('.md')]
+
+res = {}
+for fn in osfiles:
+    f = open(join('./results_os/',fn), 'rt')
+    osname = fn[:-3]
+    lines = f.readlines()
+    f.close()
+    for l in lines:
+        tmp = l.split('|')
+        if len(tmp)==8:
+            t = tmp[1]
+            if '[' in t:
+                t = t[t.index('[')+1:t.index(']')]
+            t = t.strip()
+
+            if t not in res:
+                res[t] = []
+            try:
+                res[t].append([osname, float(tmp[3])])
+            except:
+                pass
+
+for lang in res:
+    if len(res[lang]) != 0:
+        print('-------------------')
+        print(lang,)
+        print('-------------------')
+        s = sorted(res[lang], key=lambda x:x[1])
+        for ss in s:
+            print(ss)
+        print('-------------------')
